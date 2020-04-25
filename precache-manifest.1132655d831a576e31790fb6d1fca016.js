7cd64ac00ee6115383c0bea2af99033f82340211
AndreaChumioque <aichumioque@gmail.com>
2020-04-25 15:59:54 -0500

    Updates

---
+++ precache-manifest.1132655d831a576e31790fb6d1fca016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e5257368e094551afad550379900d0",
+    "url": "/crehana-challenge-react/index.html"
+  },
+  {
+    "revision": "681e232ca3621cf0306b",
+    "url": "/crehana-challenge-react/static/css/main.d6648629.chunk.css"
+  },
+  {
+    "revision": "1d82a5e8aa6c83f4cde9",
+    "url": "/crehana-challenge-react/static/js/2.1bdda2a7.chunk.js"
+  },
+  {
+    "revision": "5138816a4f7c2500d317a76e49663e2d",
+    "url": "/crehana-challenge-react/static/js/2.1bdda2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681e232ca3621cf0306b",
+    "url": "/crehana-challenge-react/static/js/main.33990846.chunk.js"
+  },
+  {
+    "revision": "4c877b96b7ba7f33317a",
+    "url": "/crehana-challenge-react/static/js/runtime-main.4f6d335e.js"
+  },
+  {
+    "revision": "34e4b8cb69a22455d5dab4c754b21a9f",
+    "url": "/crehana-challenge-react/static/media/loader.34e4b8cb.svg"
+  }
+]);
